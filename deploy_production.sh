e38a4bf35ace353d31b696a59c2f8f31cfacae3e
exampleuser667 <exampleuser667@gmail.com>
2021-10-03 14:28:25 +0530

    bug fix

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,3 +1,5 @@
+sudo docker stop python-web-server-staging
+sudo docker rm python-web-server-staging
 sudo docker stop python-web-server-production
 sudo docker rm python-web-server-production
 sudo docker run -d --network host --name python-web-server-production python-web-server-$1
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,3 +1,5 @@
 sudo docker stop python-web-server-staging
 sudo docker rm python-web-server-staging
+sudo docker stop python-web-server-production
+sudo docker rm python-web-server-production
 sudo docker run -d --network host --name python-web-server-staging python-web-server-$1
